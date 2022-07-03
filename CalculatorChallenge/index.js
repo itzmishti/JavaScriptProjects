bc84e2f186d6697163edaa68acf661dfa65eb496
itzmishti <vgupta4_be20@thapar.edu>
2022-07-03 20:17:39 +0530

    changes

--- CalculatorChallenge/index.js
+++ CalculatorChallenge/index.js
@@ -1,26 +1,27 @@
 //alert("hey")
 let num1=6;
 let num2=5;
-
+// const num1 = parseInt(prompt('Enter the first number '));
+// const num2 = parseInt(prompt('Enter the second number '));
 document.getElementById("num1-el").textContent=num1;
 document.getElementById("num2-el").textContent=num2;
 
-add()
+function add()
 {
   let result =num1+num2;
-    document.getElementById("res").textContent="Sum : "+result;
+    document.getElementById("res").textContent="Sum : "+ result;
 }
-sub()
+function sub()
 {
   let result= num1-num2;
     document.getElementById("res").textContent="Difference : "+result;
 }
-mul()
+function mul()
 {
   let result =num1*num2;
     document.getElementById("res").textContent="Product : "+result;
 }
-divide()
+function divide()
 {
   let result= num1/num2;
     document.getElementById("res").textContent="Division : "+result;
